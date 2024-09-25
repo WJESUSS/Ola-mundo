cb0a4f66ef6a2d974e8c36f60dbdf58c985b7ae5
WJESUSS <162203970+WJESUSS@users.noreply.github.com>
2024-09-25 12:09:08 -0300

    JavaTreinamento

---
+++ JavaTreiamento.java
@@ -0,0 +1,11 @@
+/**
+ * JavaTreiamento
+ */
+public class JavaTreiamento {
+   
+    public static void main(String[] args) {
+       
+        System.out.println("Washington jesus");
+    }
+    
+}
